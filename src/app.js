82620d6f200c23680c9e1e5bc7f5112d56250f98
snhendo <snhllama74@sbcglobal.net>
2017-04-08 23:04:34 -0400

    board stuff

--- src/app.js
+++ src/app.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import Tile from './tile';
 import Board from './board';
 
+var tiles = [];
+for(var i=0; i<5; i++){
+  var row = [];
+  for (var j=0; j<5; j++){
+    row.push({title: `tile ${i+j}`, description: 'this is tile'});
+  }
+  tiles.push(row);
+}
+
+
+
 class App extends React.Component {
   render() {
     return(
       <div>
-        <Board arr= />
+        <Board
+          tiles={tiles}
+         />
       </div>
     );
   }
--- src/board.js
+++ src/board.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Tile from './tile';
 
-class Board extends React.Component {
+
+export default class Board extends React.Component {
   render() {
     return(
-      <div>{this.props.message}</div>
+      <div>{
+        this.props.tiles.map(function(row){
+          return (<div> {row.map(function(item, counter){
+            return <Tile
+              title={item.title}
+              className="col-xs-2"
+            />
+          })}</div>);
+        })
+      };</div>
     );
   }
 }
